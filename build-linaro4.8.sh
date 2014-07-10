e004c759aa10cb378e3f632caf9f6e1a835bdef3
shivamk11 <shivamk11@gmail.com>
2014-07-10 13:58:30 +0530

    script: Fix it for falcon Signed-off-by: shivamk11 <shivamk11@gmail.com>

--- build-linaro4.8.sh
+++ build-linaro4.8.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 TOOLCHAIN="/home/shivam/development/toolchains/linaro-4.8-generic/bin/arm-gnueabi"
 MODULES_DIR="/home/shivam/development/modules"
-ZIMAGE="/home/shivam/development/android_kernel_motorola_msm8226/arch/arm/boot/zImage"
-KERNEL_DIR="/home/shivam/development/android_kernel_motorola_msm8226"
-if [ -a $KERNEL_DIR/arch/arm/boot/zImage ];
+ZIMAGE="/home/shivam/development/android_kernel_mototola_msm8226/arch/arm/boot/zImage-dtb"
+KERNEL_DIR="/home/shivam/development/android_kernel_mototola_msm8226"
+if [ -a $ZIMAGE ];
 then
 rm $ZIMAGE
 rm $MODULES_DIR/*
 fi
 make ARCH=arm CROSS_COMPILE=$TOOLCHAIN- msm8226_mmi_defconfig
 make ARCH=arm CROSS_COMPILE=$TOOLCHAIN- -j8
-if [ -a $KERNEL_DIR/arch/arm/boot/zImage ];
+if [ -a $ZIMAGE ];
 then
 echo "Copying modules"
 rm $MODULES_DIR/*
