025ecd6ac16446f0fe1c029e28df6b183e604428
shivamk11 <shivamk11@gmail.com>
2014-08-21 12:17:30 +0530

    clean.sh: Clean these also

--- clean.sh
+++ clean.sh
@@ -1,1 +1,6 @@
-make ARCH=arm CROSS_COMPILE=/home/shivam/development/toolchains/linaro-4.8-generic/bin/arm-gnueabi- -j8 clean mrproper
+KERNEL_DIR="/home/shivam/development/android_kernel_mototola_msm8226"
+make ARCH=arm CROSS_COMPILE=/home/shivam/development/toolchains/linaro-4.9-generic/bin/arm-eabi- -j8 clean mrproper
+rm -rf $KERNEL_DIR/ramdisk.cpio
+rm -rf $KERNEL_DIR/root.fs
+rm -rf $KERNEL_DIR/boot.img
+rm -rf $KERNEL_DIR/dt.img
